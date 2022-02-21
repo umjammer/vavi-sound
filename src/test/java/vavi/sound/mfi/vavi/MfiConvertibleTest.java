5fdb0baf7212617bc3e9da8bf6c47c1fd1538c1d
Naohide Sano <namachapanda2005@gmail.com>
2022-02-21 12:05:28 +0900

    [tests] add more

---
+++ src/test/java/vavi/sound/mfi/vavi/MfiConvertibleTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2022 by Naohide Sano, All rights reserved.
+ *
+ * Programmed by Naohide Sano
+ */
+
+package vavi.sound.mfi.vavi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+
+/**
+ * MfiConvertibleTest.
+ *
+ * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (umjammer)
+ * @version 0.00 2022/02/16 umjammer initial version <br>
+ */
+class MfiConvertibleTest {
+
+    @Test
+    void test() {
+        // exists in /vavi/sound/mfi/vavi/vavi.properties
+        MfiConvertible converter = MfiConvertible.factory.get("short.144");
+        assertInstanceOf(VaviNoteMessage.class, converter);
+        // not exists
+        converter = MfiConvertible.factory.get("meta.144");
+        assertNull(converter);
+    }
+}
+
+/* */
--- src/test/java/vavi/sound/mfi/vavi/sequencer/AudioEngineTest.java
+++ src/test/java/vavi/sound/mfi/vavi/sequencer/AudioEngineTest.java
@@ -8,9 +8,14 @@ package vavi.sound.mfi.vavi.sequencer;
 
 import org.junit.jupiter.api.Test;
 
+import vavi.sound.mobile.AudioEngine;
 import vavi.sound.mobile.AudioEngine.Util;
+import vavi.sound.mobile.FuetrekAudioEngine;
+import vavi.sound.mobile.RohmAudioEngine;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 
 /**
@@ -35,6 +40,19 @@ public class AudioEngineTest {
         byte[] monoR = new byte[] { (byte) 0x12, (byte) 0x34 };
         byte[] stereo = Util.toStereo(monoL, monoR, 4, null);
         assertArrayEquals(new byte[] { (byte) 0xa1, (byte) 0xb2, (byte) 0xc3, (byte) 0xd4 }, stereo);
+    }
+
+
+    @Test
+    public void test3() {
+        // exists in /vavi/sound/mfi/vavi/vavi.properties
+        AudioEngine audioEngine = AudioDataSequencer.Factory.getAudioEngine(0x80);
+        assertInstanceOf(RohmAudioEngine.class, audioEngine);
+        audioEngine = AudioDataSequencer.Factory.getAudioEngine(0x81);
+        assertInstanceOf(FuetrekAudioEngine.class, audioEngine);
+        // not exists
+        audioEngine = AudioDataSequencer.Factory.getAudioEngine(0x82);
+        assertNull(audioEngine);
     }
 }
 
