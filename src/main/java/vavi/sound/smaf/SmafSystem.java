10d9f9c58d300c824dda99bd36d62b4f274ece23
Naohide Sano <namachapanda2005@gmail.com>
2026-03-18 18:31:42 +0900

    [smaf] make it compliant with the specs.

--- src/main/java/vavi/sound/smaf/SmafSystem.java
+++ src/main/java/vavi/sound/smaf/SmafSystem.java
@@ -31,27 +31,12 @@ public final class SmafSystem {
 
     private static final Logger logger = getLogger(SmafSystem.class.getName());
 
-    /** TODO SmafDeviceProvider */
-    private static final Sequencer sequencer;
-    /** TODO SmafDeviceProvider */
-    private static final SmafMidiConverter converter;
-    /** TODO SmafDeviceProvider */
-    private static final SmafSynthesizer synthesizer;
-    /** */
-    private static final SmafFileReader reader;
-    /** */
-    private static final SmafFileWriter writer;
-
-    /* */
-    static {
-        // TODO SmafDeviceProvider
-        sequencer = new SmafSequencer();
-        converter = new SmafMidiConverter();
-        synthesizer = new SmafSynthesizer();
-
-        reader = new SmafFileReader();
-        writer = new SmafFileWriter();
-    }
+    /** TODO spi */
+    private static SmafDeviceProvider deviceProvider = new SmafDeviceProvider();
+    /** TODO spi */
+    private static final SmafFileReader reader = new SmafFileReader();
+    /** TODO spi */
+    private static final SmafFileWriter writer = new SmafFileWriter();
 
     /** */
     private SmafSystem() {
@@ -65,6 +50,7 @@ public final class SmafSystem {
 
     /** Gets a sequencer. */
     public static Sequencer getSequencer(boolean connected) throws SmafUnavailableException {
+        SmafSequencer sequencer = (SmafSequencer) deviceProvider.getDevice(SmafSequencer.info);
         if (connected) {
             try {
                 sequencer.open();
@@ -77,9 +63,9 @@ public final class SmafSystem {
     }
 
     /** Gets a listener to attach to a MIDI sequencer. */
-    public static Synthesizer getSynthesizer()
-        throws SmafUnavailableException {
+    public static Synthesizer getSynthesizer() throws SmafUnavailableException {
 
+        SmafSynthesizer synthesizer = (SmafSynthesizer) deviceProvider.getDevice(SmafSynthesizer.info);
         return synthesizer;
     }
 
@@ -87,8 +73,7 @@ public final class SmafSystem {
      * @param is sequencer
      * @return SmafFileFormat
      */
-    public static SmafFileFormat getSmafFileFormat(InputStream is)
-        throws InvalidSmafDataException, IOException {
+    public static SmafFileFormat getSmafFileFormat(InputStream is) throws InvalidSmafDataException, IOException {
 
         SmafFileFormat ff = reader.getSmafFileFormat(is);
         return ff;
@@ -98,16 +83,14 @@ public final class SmafSystem {
      * @param is sequencer
      * @return SMAF Sequence
      */
-    public static vavi.sound.smaf.Sequence getSequence(InputStream is)
-        throws InvalidSmafDataException, IOException {
+    public static vavi.sound.smaf.Sequence getSequence(InputStream is) throws InvalidSmafDataException, IOException {
 
         SmafFileFormat ff = getSmafFileFormat(is);
         return ff.getSequence();
     }
 
     /** Gets a SMAF sequence. */
-    public static vavi.sound.smaf.Sequence getSequence(File file)
-        throws InvalidSmafDataException, IOException {
+    public static vavi.sound.smaf.Sequence getSequence(File file) throws InvalidSmafDataException, IOException {
 
         return getSequence(new BufferedInputStream(Files.newInputStream(file.toPath())));
     }
@@ -116,10 +99,10 @@ public final class SmafSystem {
      * @param sequence SMAF Sequence
      * @return MIDI Sequence
      */
-    public static Sequence toMidiSequence(vavi.sound.smaf.Sequence sequence)
-        throws InvalidSmafDataException, SmafUnavailableException {
+    public static Sequence toMidiSequence(vavi.sound.smaf.Sequence sequence) throws InvalidSmafDataException, SmafUnavailableException {
 
         try {
+            SmafMidiConverter converter = (SmafMidiConverter) deviceProvider.getDevice(SmafMidiConverter.info);
             return converter.convert(sequence);
         } catch (IOException | InvalidMidiDataException e) {
 logger.log(Level.ERROR, e.getMessage(), e);
@@ -132,10 +115,10 @@ logger.log(Level.ERROR, e.getMessage(), e);
      * @param fileType SMAF file type
      * @return SMAF sequence
      */
-    public static vavi.sound.smaf.Sequence toSmafSequence(Sequence in, int fileType)
-        throws InvalidMidiDataException, SmafUnavailableException {
+    public static vavi.sound.smaf.Sequence toSmafSequence(Sequence in, int fileType) throws InvalidMidiDataException, SmafUnavailableException {
 
         try {
+            SmafMidiConverter converter = (SmafMidiConverter) deviceProvider.getDevice(SmafMidiConverter.info);
             return converter.convert(in, fileType);
         } catch (IOException | InvalidSmafDataException e) {
 logger.log(Level.ERROR, e.getMessage(), e);
@@ -149,15 +132,13 @@ logger.log(Level.ERROR, e.getMessage(), e);
      * @param out stream to output
      * @return SMAF sequence
      */
-    public static int write(vavi.sound.smaf.Sequence smafSequence, int fileType, OutputStream out)
-        throws IOException {
+    public static int write(vavi.sound.smaf.Sequence smafSequence, int fileType, OutputStream out) throws IOException {
 
         return writer.write(smafSequence, fileType, out);
     }
 
     /** Writes as SMAF or MIDI. */
-    public static int write(vavi.sound.smaf.Sequence smafSequence, int fileType, File out)
-        throws IOException {
+    public static int write(vavi.sound.smaf.Sequence smafSequence, int fileType, File out) throws IOException {
 
         return writer.write(smafSequence, fileType, out);
     }
